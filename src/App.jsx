72bb2431e40c74edbd87739d3b962eb7ced9ff53
adhlira <adhliramadhan68@gmail.com>
2024-09-21 12:23:50 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-xl'>Halo ini adalah halaman awal</p>
+    </>
+  )
+}
+
+export default App
